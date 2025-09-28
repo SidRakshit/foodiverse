cbc85e1e40cb87429281b01f9b6b6f7c84956730
dohhyunoh <165604799+dohhyunoh@users.noreply.github.com>
2025-09-28 02:10:49 -0400

    remove .next build artifacts

--- frontend/.next/static/chunks/frontend_app_layout_tsx_e0f40927._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__b2a040c4._.css",
-  "static/chunks/814d3_next_dist_c3cb36c4._.js"
-],
-    source: "dynamic"
-});
--- frontend/.next/static/chunks/frontend_app_page_tsx_fbc6cbb3._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/frontend_app_1f500ddc._.js",
-  "static/chunks/9e883__pnpm_f2229574._.js"
-],
-    source: "dynamic"
-});
